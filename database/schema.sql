55892c68e91d0b5324cf9b8e3dce219e8c5891c6
Alex Wang <alxswang@umich.edu>
2024-07-07 11:13:58 -0700

    added instructors table to schema

--- database/schema.sql
+++ database/schema.sql
@@ -7,3 +7,10 @@ CREATE TABLE courses(
 CREATE TABLE users(
 
 );
+
+CREATE TABLE instructors(
+  instructor_id INT AUTO_INCREMENT PRIMARY KEY,
+  full_name VARCHAR(256) NOT NULL,
+  department VARCHAR(256) NOT NULL,
+  email VARCHAR(256) UNIQUE
+);
