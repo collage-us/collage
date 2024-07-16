5281b316cbf5cb3e6d1a5f373ef1f1356e9ce75b
Zichen Zhang <zhangzzc@umich.edu>
2024-07-15 21:33:03 -0400

    remove API key

--- collage/server/dalle.py
+++ collage/server/dalle.py
@@ -2,10 +2,13 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+# load_dotenv()
 
-api_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=api_key)
+# api_key = os.getenv("OPENAI_API_KEY")
+# client = OpenAI(api_key=api_key)
+
+# TODO: for CI/CD
+client = OpenAI()
 
 def generate_image(model, prompt):
     response = client.images.generate(
