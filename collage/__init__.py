804bfb42b48ae152116295259ca7f7201802a05a
Zichen Zhang <zhangzzc@umich.edu>
2024-07-18 15:50:49 -0400

    Set up Heroku CI/CD

--- collage/__init__.py
+++ collage/__init__.py
@@ -13,6 +13,15 @@ app.config.from_envvar('COLLAGE_SETTINGS', silent=True)
 
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "your_secret_key")
 
+def create_app():
+    app = flask.Flask(__name__, static_folder='../static', template_folder='../templates')
+
+    # Import and register your blueprints here
+    from collage.views import index_bp
+    app.register_blueprint(index_bp)
+
+    return app
+
 import collage.server
 import collage.views
 # import collage.model
