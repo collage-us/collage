57cde121dfec2052b141798d19863b214249ba4e
Zichen Zhang <zhangzzc@umich.edu>
2024-07-22 10:14:21 -0400

    omit nltk data downloading for Heroku

--- collage/server/nltk_utils.py
+++ collage/server/nltk_utils.py
@@ -4,19 +4,25 @@ import os
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-# Define the writable directory for NLTK data
-nltk_data_dir = os.path.expanduser('~/nltk_data')
+# # Define the writable directory for NLTK data
+# nltk_data_dir = os.path.expanduser('~/nltk_data')
 
-# Create the directory if it doesn't exist
-os.makedirs(nltk_data_dir, exist_ok=True)
+# # Create the directory if it doesn't exist
+# os.makedirs(nltk_data_dir, exist_ok=True)
 
-# Set the NLTK data path to the new directory
-nltk.data.path.append(nltk_data_dir)
+# # Set the NLTK data path to the new directory
+# nltk.data.path.append(nltk_data_dir)
 
-# Download the necessary NLTK data
-nltk.download('punkt', download_dir=nltk_data_dir)
-nltk.download('stopwords', download_dir=nltk_data_dir)
+# # Function to safely download NLTK data
+# def safe_nltk_download(package):
+#     try:
+#         nltk.download(package, download_dir=nltk_data_dir)
+#     except Exception as e:
+#         print(f"Error downloading {package}: {e}")
 
+# # Download the necessary NLTK data
+# safe_nltk_download('punkt')
+# safe_nltk_download('stopwords')
 
 def parse_resume(pdf_file):
     """Return keywords from the resume input from the user when the user signs up."""
